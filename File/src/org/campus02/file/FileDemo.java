955416eeea797fde96158d7332eed5ffdbf72db7
czell <czell@UCL08248.knapp.at>
2016-04-16 14:43:51 +0200

    check in

---
+++ File/src/org/campus02/file/FileDemo.java
@@ -0,0 +1,15 @@
+package org.campus02.file;
+
+import java.io.File;
+
+public class FileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		File dir = new File("D:\\temp", "temp.txt");
+		System.out.println(dir.isFile() + " , " + dir.getPath() + " " + dir.canWrite() + " " + dir.canRead() + " " + dir.exists() + " " + dir.length());
+		
+	}
+
+}
