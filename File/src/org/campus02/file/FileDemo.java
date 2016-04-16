23b32974a317b981c851221b32c6d04c6a120dd7
czell <czell@UCL08248.knapp.at>
2016-04-16 14:48:47 +0200

    cd

--- File/src/org/campus02/file/FileDemo.java
+++ File/src/org/campus02/file/FileDemo.java
@@ -7,7 +7,7 @@ public class FileDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		File dir = new File("D:\\temp", "temp.txt");
+		File dir = new File("D:\\temp");
 		System.out.println(dir.isFile() + " , " + dir.getPath() + " " + dir.canWrite() + " " + dir.canRead() + " " + dir.exists() + " " + dir.length());
 		
 	}
